19f8c01c766358bba07e871bf7dc1255e8d4882f
sandas <sandazzzg1999@gmail.com>
2026-08-05 23:39:32 +0530

    chore(web): configure http client, structure and environment

---
+++ frontend/src/app/components/expense-summary/expense-summary.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-summary',
+  imports: [],
+  templateUrl: './expense-summary.html',
+  styleUrl: './expense-summary.css',
+})
+export class ExpenseSummary {}
